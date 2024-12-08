a7db68ab2f6353e4255751e87b2d4c214f1664b3
powerfuljackell <johnsonjakel@aol.com>
2024-12-07 17:43:27 -0700

    updated graphGenerator.js

--- graphGenerator.js
+++ graphGenerator.js
@@ -1,4 +1,4 @@
-function graphMatrixGenerator(n){
+function graphMatrixMaker(n){
     matrix = [...Array(n)].map(_=>Array(n).fill(0));
     for(i = 0; i < n; i++){ //n complexity
         for(j = i; j <n; j++ ){//1/2n average
@@ -11,4 +11,53 @@ function graphMatrixGenerator(n){
         }
     }
     return matrix;
+}
+
+function graphListMaker(n){
+    matrix = [...Array(n)].map(_=>Array());
+    for(i = 0; i < n; i++){ //n complexity
+        for(j = i; j <n; j++ ){//1/2n average
+            if(i != j){
+                if(Math.random() >= 0.5){
+                    matrix[i].push(j)
+                    matrix[j].push(i)
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+function* graphMatrixGenerator(n){
+    while(true){
+        matrix = [...Array(n)].map(_=>Array(n).fill(0));
+        for(i = 0; i < n; i++){ //n complexity
+            for(j = i; j <n; j++ ){//1/2n average
+                if(matrix[i][j] == 0 && i !=j){
+                    if(Math.random() >= 0.5){
+                        matrix[i][j] = 1
+                        matrix[j][i] = 1
+                    }
+                }
+            }
+        }
+        yield matrix;
+    }
+}
+
+function* graphListGenerator(n){
+    while(true){
+        matrix = [...Array(n)].map(_=>Array());
+        for(i = 0; i < n; i++){ //n complexity
+            for(j = i; j <n; j++ ){//1/2n average
+                if(i != j){
+                    if(Math.random() >= 0.5){
+                        matrix[i].push(j)
+                        matrix[j].push(i)
+                    }
+                }
+            }
+        }
+        yield matrix;
+    }
 }
